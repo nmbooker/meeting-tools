07b3878b9df53a60194f925eb02a233e4a78ce8a
Nick Booker <nick@sunfieldtech.net>
2011-09-26 15:41:13 +0100

    Refactored processing into MinutesProcessor class.

--- minutes.rb
+++ minutes.rb
@@ -3,6 +3,31 @@
 require 'optparse'
 require_relative 'numbering'
 
+class MinutesProcessor
+  def initialize(options)
+    @options = options
+  end
+
+  def process_minutes
+    title_number = Numbering.new
+    for line in $stdin
+      num = case line
+        when /^====/ then title_number.h4
+        when /^===/ then title_number.h3
+        when /^==/ then title_number.h2
+        when /^=/ then title_number.h1
+        else nil
+      end
+      if num.nil?
+        print line
+      else
+        print "\n\n\n\n\n\n" if @options[:notes]
+        print line.sub(/^=+/, "#{num}.")
+      end
+    end
+  end
+end
+
 def main
   options = {}
   OptionParser.new do |opts|
@@ -12,23 +37,7 @@ def main
       options[:notes] = n
     end
   end.parse!
-
-  title_number = Numbering.new
-  for line in $stdin
-    num = case line
-      when /^====/ then title_number.h4
-      when /^===/ then title_number.h3
-      when /^==/ then title_number.h2
-      when /^=/ then title_number.h1
-      else nil
-    end
-    if num.nil?
-      print line
-    else
-      print "\n\n\n\n\n\n" if options[:notes]
-      print line.sub(/^=+/, "#{num}.")
-    end
-  end
+  MinutesProcessor.new(options).process_minutes
 end
 
 main if $PROGRAM_NAME == __FILE__
