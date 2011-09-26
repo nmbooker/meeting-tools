76eef199a68af8afe888bc308aac1a5456e61d95
Nick Booker <nick@sunfieldtech.net>
2011-09-26 15:29:09 +0100

    Shift main code into 'main' method.

--- minutes.rb
+++ minutes.rb
@@ -36,28 +36,32 @@ class Numbering
   end
 end
 
-options = {}
-OptionParser.new do |opts|
-  opts.banner = "Usage: #{$PROGRAM_NAME} [options] < minutes.creole"
+def main
+  options = {}
+  OptionParser.new do |opts|
+    opts.banner = "Usage: #{$PROGRAM_NAME} [options] < minutes.creole"
 
-  opts.on("-n", "--notespace", "Format with space for notes") do |n|
-    options[:notes] = n
-  end
-end.parse!
+    opts.on("-n", "--notespace", "Format with space for notes") do |n|
+      options[:notes] = n
+    end
+  end.parse!
 
-title_number = Numbering.new
-for line in $stdin
-  num = case line
-    when /^====/ then title_number.h4
-    when /^===/ then title_number.h3
-    when /^==/ then title_number.h2
-    when /^=/ then title_number.h1
-    else nil
-  end
-  if num.nil?
-    print line
-  else
-    print "\n\n\n\n\n\n" if options[:notes]
-    print line.sub(/^=+/, "#{num}.")
+  title_number = Numbering.new
+  for line in $stdin
+    num = case line
+      when /^====/ then title_number.h4
+      when /^===/ then title_number.h3
+      when /^==/ then title_number.h2
+      when /^=/ then title_number.h1
+      else nil
+    end
+    if num.nil?
+      print line
+    else
+      print "\n\n\n\n\n\n" if options[:notes]
+      print line.sub(/^=+/, "#{num}.")
+    end
   end
 end
+
+main if $PROGRAM_NAME == __FILE__
