1dc520b83bb2e1a6b19294246683e3e9a5469d3a
Nick Booker <nick@sunfieldtech.net>
2011-09-26 15:47:41 +0100

    Extracted method 'process_heading_line'

--- minutes.rb
+++ minutes.rb
@@ -6,23 +6,32 @@ require_relative 'numbering'
 class MinutesProcessor
   def initialize(options)
     @options = options
+    @title_number = Numbering.new
+  end
+
+  def process_heading_line(line)
+    num = case line
+      when /^====/ then @title_number.h4
+      when /^===/ then @title_number.h3
+      when /^==/ then @title_number.h2
+      when /^=/ then @title_number.h1
+      else nil
+    end
+    if num.nil?
+      print line
+    else
+      print "\n\n\n\n\n\n" if @options[:notes]
+      print line.sub(/^=+/, "#{num}.")
+    end
   end
 
   def process_minutes
     title_number = Numbering.new
     for line in $stdin
-      num = case line
-        when /^====/ then title_number.h4
-        when /^===/ then title_number.h3
-        when /^==/ then title_number.h2
-        when /^=/ then title_number.h1
-        else nil
-      end
-      if num.nil?
-        print line
+      if line =~ /^=/ then
+        process_heading_line(line)
       else
-        print "\n\n\n\n\n\n" if @options[:notes]
-        print line.sub(/^=+/, "#{num}.")
+        print line
       end
     end
   end
