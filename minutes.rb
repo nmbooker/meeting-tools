57b6903f892320f45d29b5f8b89d3eb93d856884
Nick Booker <nick@sunfieldtech.net>
2011-11-25 12:47:56 +0000

    Accept source and output filenames on command line.

--- minutes.rb
+++ minutes.rb
@@ -10,6 +10,12 @@ class MinutesProcessor
     @already_in_todo_list = false
   end
 
+  protected
+  def out
+    @options[:outfile]
+  end
+
+  public
   def process_heading_line(line)
     num = case line
       when /^====/ then @title_number.h4
@@ -19,39 +25,39 @@ class MinutesProcessor
       else nil
     end
     if num.nil?
-      print line
+      out.print line
     else
-      print "\n\n\n" if @options[:notes]
-      print line.sub(/^=+/, "#{num}.")
+      out.print "\n\n\n" if @options[:notes]
+      out.print line.sub(/^=+/, "#{num}.")
     end
   end
 
   def process_todo_line(line)
-    puts "To Do:" unless @already_in_todo_list
+    out.puts "To Do:" unless @already_in_todo_list
     @already_in_todo_list = true
     if line =~ /^\s*TODO: ?/ then
       # Indent each To Do item by 2 spaces.
-      print line.sub(/^\s*TODO: ?/, " * ")
+      out.print line.sub(/^\s*TODO: ?/, " * ")
     else
       # Continuation of a To Do item onto a new line:
       # make the first non-whitespace character a colon
-      print line.sub(/^\s*: ?/, "       ")
+      out.print line.sub(/^\s*: ?/, "       ")
     end
   end
 
   def process_done_line(line)
-    puts "Done:" unless @already_in_done_list
+    out.puts "Done:" unless @already_in_done_list
     @already_in_done_list = true
     if line =~ /^\s*DONE: ?/ then
-      print line.sub(/^\s*DONE: ?/, "* ")
+      out.print line.sub(/^\s*DONE: ?/, "* ")
     else
-      print line.sub(/^\s*: ?/, "      ")
+      out.print line.sub(/^\s*: ?/, "      ")
     end
   end
 
   def process_minutes
     title_number = Numbering.new
-    for line in $stdin
+    for line in @options[:infile]
       if line =~ /^=/ then
         process_heading_line(line)
       elsif line =~ /^\s*TODO:/
@@ -64,7 +70,7 @@ class MinutesProcessor
         process_done_line(line)
       else
         @already_in_todo_list = @already_in_done_list = false
-        print line
+        out.print line
       end
     end
   end
@@ -73,12 +79,17 @@ end
 def main
   options = {}
   OptionParser.new do |opts|
-    opts.banner = "Usage: #{$PROGRAM_NAME} [options] < minutes.min"
+    opts.banner = "Usage: #{$PROGRAM_NAME} [options] input_filename output_filename"
 
     opts.on("-n", "--notespace", "Format with space for notes") do |n|
       options[:notes] = n
     end
   end.parse!
+  p ARGV
+  infile = ARGV[0] || '-'
+  outfile = ARGV[1] || '-'
+  options[:infile] = if infile == '-' then $stdin else File.open(infile) end
+  options[:outfile] = if outfile == '-' then $stdout else File.open(outfile, 'w') end
   MinutesProcessor.new(options).process_minutes
 end
 
