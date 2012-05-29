efe65bbb1d748522e570f1c4c35945cf2576f273
Nick Booker <NMBooker@gmail.com>
2012-05-29 12:49:18 +0100

    get_actions allows you to select individual people's TODO

--- get_actions.rb
+++ get_actions.rb
@@ -6,17 +6,33 @@ require_relative 'minutes_reader'
 
 # Minutes interpreter that just outputs current TODO items.
 class ToDoInterpreter < Meeting::MinutesInterpreter
-  def initialize
+  def initialize(options={})
     @outfile = $stdout
+    @who_filter = options[:people]
+    @who_is_it = nil
   end
 
   # To-do lists are output as bulleted lists.
   def special_list_item_line(text)
     return unless reader.in_special_list?(:TODO)
     if reader.list_item_line_number == 1
-      out.write "* " + text
+      if @who_filter
+        matchobj = text.match(/([a-zA-Z1-9\/,\?]+):/)
+        people = matchobj ? matchobj[1] : nil
+        @who_is_it = nil
+        unless people.nil?
+          #        get rid of ? marks    split on / and ,
+          people = people.gsub(/\?/, '').split(/[\/,]/)
+          @who_is_it = people.empty? ? @who_filter : people
+        end
+      end
+      if person_matches_filter
+        out.write "* " + text
+      end
     else
-      out.write "  " + text
+      if person_matches_filter
+        out.write "  " + text
+      end
     end
   end
 
@@ -34,8 +50,15 @@ class ToDoInterpreter < Meeting::MinutesInterpreter
   def out
     @outfile
   end
+
+  def person_matches_filter
+    return true unless @who_filter
+    return true if @who_is_it.nil? or @who_is_it.empty?
+    return !(@who_is_it & @who_filter).empty?
+  end
 end
 
-interpreter = ToDoInterpreter.new
+people = ARGV.empty? ? nil : ARGV
+interpreter = ToDoInterpreter.new(:people => people)
 reader = Meeting::MinutesReader.new(interpreter)
 reader.process_file($stdin)
